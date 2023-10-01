965e9b9f8f441155d91277d0ef1f5174e410e81b
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-02 00:04:58 +0530

    evaluating the performance of the model using various metrics

---
+++ evaluate.py
@@ -0,0 +1,20 @@
+from sklearn.model_selection import train_test_split
+from test import load_model
+from preprocess import prepare_dataset
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import pandas as pd
+
+model = load_model()
+df = pd.read_csv("./dataset/spam.csv")
+X, y = prepare_dataset(df)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_pred = model.predict(X_test)
+probabilites = model.predict_proba(X_test)
+predictions = (probabilites[:,1] >= 0.85).astype(int)
+print(predictions)
+print(f"Training Score : {accuracy_score(model.predict(X_train), y_train)}")
+print(f"Test Score : {accuracy_score(y_test, y_pred)}")
+print(f"Test Score on 0.85 Threshold: {accuracy_score(y_test, predictions)}")
+print(classification_report(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+
