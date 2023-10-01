f21cbba96059ec7b42ea46d5a78542c067f8eab6
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-01 12:16:11 +0530

    feature: added wordcloud for spam dataset

--- eda.py
+++ eda.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from wordcloud import WordCloud
+from math import log, sqrt
+
 def plot_frequency_count(df, column):
   """
     Plot the frequency count of the column
@@ -21,7 +27,22 @@ def groupby_data(df, column):
   else :
     return "Column not found"
   
+def wordcloud(df, category):
+  assert category in ['spam', 'ham'], "Category should be either spam or ham"
+  spam_words = " ".join(list(df[df['label'] == category]['text']))
+  spam_wc = WordCloud(width=512, height=512).generate(spam_words)
+  plt.figure(figsize=(10,8), facecolor='k')
+  plt.imshow(spam_wc)
+  plt.axis('off')
+  plt.title("Wordcloud for {}".format(category), fontsize=20)
+  plt.tight_layout(pad=0)
+  plt.savefig('./images/wordcloud_{}.png'.format(category))
+  plt.show()
+
+
 if __name__ == "__main__":
   df = pd.read_csv('./dataset/spam.csv')
   plot_frequency_count(df, 'label')
+  wordcloud(df, 'spam')
+  wordcloud(df, 'ham')
   print(groupby_data(df, 'label'))
