5c7299e7b09ca9c5645c3f6e4841a3f1b33aae78
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-09 00:09:23 +0530

    improved results on CountVectorizer

--- train.py
+++ train.py
@@ -2,20 +2,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from preprocess import prepare_dataset
 import pandas as pd
 import pickle
 
 model = Pipeline([
-  ('vectorizer', CountVectorizer(max_features=5000)),
+  ('vectorizer', CountVectorizer(max_features=15000)),
   ('nb', MultinomialNB())
 ])
 
-tf_model = Pipeline([
-  ('vectorizer', TfidfVectorizer(max_features=5000)),
-  ('nb', MultinomialNB())
-])
+# tf_model = Pipeline([
+#   ('vectorizer', TfidfVectorizer(max_features=10000)),
+#   ('nb', MultinomialNB())
+# ])
 
 def train_model(X,y, model):
   """
@@ -28,9 +28,11 @@ if __name__ == "__main__":
   df = pd.read_csv("./dataset/spam.csv")
   X, y = prepare_dataset(df)
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-  model = train_model(X_train, y_train, tf_model)
-  print(model.score(X_test, y_test))
-
-  with open('./model/model.pickle', 'wb') as file:
-    pickle.dump(model, file)
+  model = train_model(X_train, y_train, model)
+  print(f"Training Score {model.score(X_train, y_train)}")
+  print(f"Testing Score {model.score(X_test, y_test)}")
+  print(confusion_matrix(model.predict(X_test), y_test))
+  print(classification_report(model.predict(X_test), y_test))
+  # with open('./model/model.pickle', 'wb') as file:
+  #   pickle.dump(model, file)
 
